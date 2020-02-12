042092808d30bffcf35e761d8a20a59e70ae6c1f
bpaumard <benjamin53200@gmail.com>
2020-02-12 09:58:39 +0100

    initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FilActualite.Startup))]
+namespace FilActualite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
